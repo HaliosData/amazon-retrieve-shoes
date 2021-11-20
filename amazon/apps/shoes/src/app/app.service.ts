628cd82bf9c907d342604a0f6fda1599e32b545a
Gary Gautruche <service@halios-data.com>
2021-11-20 20:30:55 +0100

    feat: implementation done

--- amazon/apps/shoes/src/app/app.service.ts
+++ amazon/apps/shoes/src/app/app.service.ts
@@ -1,40 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import {Axios} from 'axios';
+import { Axios } from 'axios';
 
 @Injectable()
 export class AppService {
-private axios: Axios = null;
+  private axios: Axios = null;
 
-  constructor(){
+  constructor() {
     this.axios = new Axios({});
-    this.axios.interceptors.response.use(r=>r, this.handleAxiosError);
+    this.axios.interceptors.response.use(r => r, this.handleAxiosError);
   }
 
 
-  private handleAxiosError(err){
+  private handleAxiosError(err) {
     console.log(err);
   }
 
-  private async getProduct(asin: string){
-    console.log('test');
-    var amazonProduct = await this.axios.request({
-      method: 'GET',
-      url: 'https://amazon24.p.rapidapi.com/api/product/'.concat(asin),
-      headers: {
-        'x-rapidapi-host': 'amazon24.p.rapidapi.com',
-        'x-rapidapi-key': 'ce7b76fd45mshae4314f1df45c64p19eac4jsn232cf211ec44'
-      }
-    })
-
-    return amazonProduct;
-  }  
+  private async getProduct(asin: string, productURL: string) {
+    try {
+      var amazonProduct = await this.axios.request({
+        method: 'GET',
+        url: 'https://amazon24.p.rapidapi.com/api/product/'.concat(asin),
+        params: { productURL: productURL },
+        headers: {
+          'x-rapidapi-host': 'amazon24.p.rapidapi.com',
+          'x-rapidapi-key': 'YOUR_TOKEN'
+        }
+      })
+    } catch (e) {
+      throw e;
+    }
+    return JSON.parse(amazonProduct.data);
+  }
 
 
   async getData(): Promise<{ message: any; }> {
-    var amazonProduct = await this.getProduct('B07DBM6WG4');
-    console.log(amazonProduct);
-    const jsonText = JSON.stringify(amazonProduct);
-
-    return { message: JSON.parse(jsonText) };
+    try {
+      var amazonProduct = await this.getProduct('0', 'https://www.amazon.com/dp/B07DBHRNXB?psc=6');
+    } catch (e) {
+      throw e;
+    }
+    return { message: amazonProduct };
   }
+
 }
